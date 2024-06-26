7d59e6333a437eaf40e893fe521404efca098345
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-06-26 19:59:46 +0530

    add new futures to form

--- my-app/src/component/Navbar.js
+++ my-app/src/component/Navbar.js
@@ -21,11 +21,10 @@ export default function Navbar(props) {
           </li>
          
         </ul>
-        <form className="d-flex">
+        {/* <form className="d-flex">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success" type="submit" >{props.find}</button> 
-          {/* enter button name using props */}
-        </form>
+        </form> */}
       </div>
     </div>
   </nav>
--- my-app/src/component/TextForm.js
+++ my-app/src/component/TextForm.js
@@ -3,43 +3,77 @@ import { useState } from "react";
 
 export default function Text_editor(props) {
   const [text, setText] = useState("NewText");
+
+  const [isBold, setIsBold] = useState(false);
+  const [isitalic, setitalic] = useState(false);
+
+  function boldText() {
+    setIsBold(!isBold);
+  }
+
+  function italicText() {
+    setitalic(!isitalic);
+  }
+
   function changetext(event) {
-      console.log("you click this btn")
-     setText(event.target.value);
+    console.log("you click this btn");
+    setText(event.target.value);
   }
+
   function touppercase() {
-    let NewText=text.toUpperCase();
+    let NewText = text.toUpperCase();
     setText(NewText);
   }
+
   function tolowercase() {
-    let NewText=text.toLowerCase();
+    let NewText = text.toLowerCase();
     setText(NewText);
   }
+
+  function clearText() {
+    let NewText = "";
+    setText(NewText);
+  }
+
   return (
     <>
-    <div className="container">
-
-      <h2>{props.title}</h2>
-      <div class="mb-3">
-        <textarea
-          className="form-control"
-          rows="8"
-          value={text}
-          onChange={changetext}
+      <div className="container">
+        <h2>{props.title}</h2>
+        <div class="mb-3">
+          <textarea
+            className={`form-control ${isBold ? "bold" : ""} ${
+              isitalic ? "italic" : ""}`}
+            rows="8"
+            value={text}
+            onChange={changetext}
           ></textarea>
-        <button className="btn btn-primary m-2" onClick={touppercase}>
-          ToUppercase
-        </button>
-        <button className="btn btn-primary m-2" onClick={tolowercase}>
-          Tolowercase
-        </button>
-          </div>
+          <button className="btn btn-primary m-2" onClick={touppercase}>
+            ToUppercase
+          </button>
+          <button className="btn btn-primary m-2" onClick={tolowercase}>
+            Tolowercase
+          </button>
+          <button className="btn btn-primary m-2" onClick={clearText}>
+            Clear text
+          </button>
+          <button className="btn btn-primary m-2" onClick={boldText}>
+            Bold text
+          </button>
+          <button className="btn btn-primary m-2" onClick={italicText}>
+            italic text
+          </button>
+        </div>
       </div>
       <div className="container">
         <h2>Count</h2>
-        <p>there are {text.split(" ").length} Words and {text.length} character in this paragraph <br/> It takes {0.25 * text.split(" ").length}seconds to read</p>
+        <p>
+          there are {text.split(" ").length - 1} Words and {text.length}{" "}
+          character in this paragraph <br /> It takes{" "}
+          {0.25 * text.split(" ").length - 0.25}seconds to read
+        </p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p className={` ${isBold ? "bold" : ""} ${
+              isitalic ? "italic" : ""}`}>{text}</p>
       </div>
     </>
   );
