405f63fa50d719a371f79eb9db11cd86c9677ded
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-06-29 07:34:37 +0530

    add alert

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,39 @@
 import "./App.css";
+import Alert from "./component/Alert";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
 import { useState } from "react";
 function App() {
   const [mode, setmode] = useState('light');
   const [Text, setText] = useState('Dark');
+  const [alert,setalert]=useState();
+  const showAlert=(type ,message)=>{
+    setalert({
+      message:message,
+      type:type
+    })
+    setTimeout(() => {
+      setalert(null)
+    },2000);
+  }
   function togglemode() {
     if(mode==='light'){
       setmode('dark');
       setText('Light');
-      document.body.style.backgroundColor='gray';
+      document.body.style.backgroundColor='#1f3b51';
+      showAlert('success',' successfully turn on dark mode')
     }
     else{
       setmode('light');
       setText('Dark');
       document.body.style.backgroundColor='white';
+      showAlert('success',' successfully turn on light mode')
     }
   }
   return (
     <>
       <Navbar title="Rathod Pratik" mode={mode} btn={Text} togglemode={togglemode}/>
+      {alert && <Alert type={alert.type} message={alert.message} />}
       <div className="my-4 container " >
         <TextForm title="Enter text to check" mode={mode} />
       </div>
