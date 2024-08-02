4d3fabcb13cda6f3ae48b577531d9e769b3bd2ea
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-08-02 17:57:05 +0530

    Header

--- Contect form/src/App.jsx
+++ Contect form/src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import Navbar from './component/Navbar/Navbar';
+import ContectHeader from './component/Contect/ContectHeader';
 function App() {
   return (
+    <div>
       <Navbar/>
+      <ContectHeader/>
+    </div>
   );
 }
 
--- Contect form/src/component/Contect/ContectHeader.jsx
+++ Contect form/src/component/Contect/ContectHeader.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import style from './ContectHeader.module.css'
 
 const ContectHeader = () => {
   return (
-    <div>
-      
+    <div className={`${style.contect_section}`}>
+      <h1>Contect US</h1>
+      <p>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA</p>
     </div>
   )
 }
