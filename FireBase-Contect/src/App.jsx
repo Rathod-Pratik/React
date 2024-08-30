9a26ffa3436377db0463902106a4a716ddd325a4
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-08-30 20:37:47 +0530

    Create navbar and connect database

--- FireBase-Contect/src/App.jsx
+++ FireBase-Contect/src/App.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/FireBase";
+import { HiOutlineUserCircle } from "react-icons/hi";
 
 const App = () => {
+  const [contects, setcontects] = useState([]);
+  useEffect(() => {
+    const getcontects = async () => {
+    try{
+        const contectRef = collection(db, "Contect");
+        const contectsSnapshot = await getDocs(contectRef);
+        const ContectList = contectsSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setcontects(ContectList);
+      } catch (error) {
+      }
+    };
+    getcontects();
+  }, []);
   return (
-    <div>
-      
+    <div className="w-[370px] mx-auto px-4">
+      <Navbar />
+      <div className="flex gap-2">
+        <div className="flex items-center relative my-2">
+          <FiSearch className="absolute text-white ml-1 text-3xl" />
+          <input
+            type="text"
+            className="rounded-md h-10 border text-white border-white bg-transparent pl-9 flex-grow w-[295px]"
+            placeholder="Search Contect"
+          />
+        </div>
+        <div className="flex items-center">
+          <AiFillPlusCircle className="text-white text-5xl cursor-pointer" />
+        </div>
+      </div>
+      <div>
+        {contects.map((contect) => (
+          <div key={contect.id}>
+            <HiOutlineUserCircle />
+            <div className="text-white">
+              <h2 className="">{contect.name}</h2>
+              <p className="">{contect.email}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
