e71aa6c64688abea8ac893c7c29d67ce5d25488f
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-10-02 17:16:41 +0530

    update

--- iNotebook/Backend/index.js
+++ iNotebook/Backend/index.js
@@ -1,4 +1,4 @@
-const {connnectToMongo}=require('./connection');
+const {connnectToMongo}=require('../functions/connection');
 connnectToMongo("mongodb+srv://RathodPratik:Pratik%401432@cluster0.v5r2m.mongodb.net/iNotebook?retryWrites=true&w=majority").then(()=>{
   console.log("connection success");
 })
@@ -10,17 +10,26 @@ const port = 5000
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin:["https://fabulous-longma-345254.netlify.app"],
+    methods:["POST","PUT","DELETE","GET"],
+    credentials:true
+  }
+))
 
 app.get("/",(req,res)=>{
-  console.log("Application running properly");
+ return res.json("server is running ");
 })
 
 // Available Routes
- app.use('/api/auth', require('./routers/auth'))
- app.use('/api/notes', require('./routers/notes'))
+app.use('/api/auth', require('../functions/auth'))
+ app.use('/api/notes', require('../functions/notes'))
 
 
+ const serverless = require('serverless-http');
+module.exports.handler = serverless(app);
+/*
 app.listen(port, () => {
   console.log(`iNotebook listening at http://localhost:${port}`)
-})
+})*/
