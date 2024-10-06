c3275ff3bdcaee8b95b99bac99a8ddcf68d9f82c
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-10-06 16:57:03 +0530

    Update NotFoundContect.jsx

--- FireBase-Contect/src/Components/NotFoundContect.jsx
+++ FireBase-Contect/src/Components/NotFoundContect.jsx
@@ -4,7 +4,7 @@ const NotFoundContect = () => {
   return (
     <div className='flex justify-center items-center h-[80vh] gap-4'>
         <div>
-      <img src="/public/Images/Contect.png" alt="" />
+      <img src="/Images/Contect.png" alt="" />
         </div>
       <h3 className='text-white text-2xl font-semibold'>Contect Not Found</h3>
     </div>
