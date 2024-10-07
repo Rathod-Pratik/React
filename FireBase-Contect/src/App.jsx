880cb7fc450a64fbaa5028382c73a420b5512dc8
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-10-07 19:13:15 +0530

    wrap code

--- FireBase-Contect/src/App.jsx
+++ FireBase-Contect/src/App.jsx
@@ -1,90 +1,20 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "./Components/Navbar";
-import { FiSearch } from "react-icons/fi";
-import { AiFillPlusCircle } from "react-icons/ai";
-import { collection, getDocs, onSnapshot } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { db } from "./config/FireBase";
-import ContectCard from "./Components/ContectCard";
-import Model from "./Components/Model";
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import AddAndUpdateContect from "./Components/AddAndUpdateContect";
-import useDisclous from "./useDisclous";
-import NotFoundContect from "./Components/NotFoundContect";
+import Signup from './Components/signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Home from './Components/Home';
 const App = () => {
-  const [contects, setcontects] = useState([]);
-  const { isOpen, onClose, onOpen } = useDisclous();
 
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        const contactRef = collection(db, "contects");
-
-        onSnapshot(contactRef, (snapshot) => {
-          const contactList = snapshot.docs.map((doc) => {
-            return {
-              id: doc.id,
-              ...doc.data(),
-            };
-          });
-          setcontects(contactList);
-        });
-      } catch (error) {
-        console.error("Error fetching contacts:", error);
-      }
-    };
-
-    getContacts();
-  }, []);
-
-  const filterContects=(e)=>{
-    const value =e.target.value;
-    const contactRef = collection(db, "contects");
-
-    onSnapshot(contactRef, (snapshot) => {
-      const contactList = snapshot.docs.map((doc) => {
-        return {
-          id: doc.id,
-          ...doc.data(),
-        };
-      });
-      
-      const filterContect=contactList.filter(contect=>contect.name.toLowerCase().includes(value.toLowerCase()))
-      setcontects(filterContect);
-      return filterContect;
-    })
-  }
 
   return (
     <>
-      <div className="mx-auto w-[370px] px-4">
-        <Navbar />
-        <div className="flex gap-2">
-          <div className="relative my-2 flex items-center">
-            <FiSearch className="absolute ml-1 text-3xl text-white" />
-            <input
-            onChange={filterContects}
-              type="text"
-              className="h-10 w-[295px] flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
-              placeholder="Search Contect"
-            />
-          </div>
-          <div className="flex items-center">
-            <AiFillPlusCircle
-              className="cursor-pointer text-5xl text-white"
-              onClick={onOpen}
-            />
-          </div>
-        </div>
-        <div>
-          {contects.length<=0 ?<NotFoundContect/>: contects.map((contect) => (
-            <ContectCard contect={contect} key={contect.id} />
-          ))}
-        </div>
-      </div>
-      <AddAndUpdateContect isOpen={isOpen} onClose={onClose} />
-      <ToastContainer position="bottom-center"/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+    </Router>
     </>
   );
 };
--- FireBase-Contect/src/Components/AddAndUpdateContect.jsx
+++ FireBase-Contect/src/Components/AddAndUpdateContect.jsx
@@ -19,7 +19,15 @@ const AddAndUpdateContect = ({ isOpen, onClose, isupdate, contect }) => {
   const addContect = async (contect) => {
     try {
       const contectRef = collection(db, "contects");
-      await addDoc(contectRef, contect);
+
+      const data={
+        name:contect.name,
+        email:contect.email,
+        mobile:contect.mobile,
+        _id:localStorage.getItem("id")
+
+      }
+      await addDoc(contectRef, data);
       onClose();
       toast.success("content Added successfully");
     } catch (error) {
@@ -63,21 +71,21 @@ const AddAndUpdateContect = ({ isOpen, onClose, isupdate, contect }) => {
           <Form className="my-4 flex flex-col gap-4">
             <div className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
-              <Field name="name" className="h-10 border" />
+              <Field name="name" className="h-10 border px-2" />
               <div className="text-red-500">
                 <ErrorMessage name="name" />
               </div>
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email</label>
-              <Field name="email" className="h-10 border" />
+              <Field name="email" className="h-10 border px-2" />
               <div className="text-red-500">
                 <ErrorMessage name="email" />
               </div>
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="mobile">Number</label>
-              <Field name="mobile" className="h-10 border" />
+              <Field name="mobile" className="h-10 border ppx-2" />
               <div className="text-red-500">
                 <ErrorMessage name="mobile" />
               </div>
