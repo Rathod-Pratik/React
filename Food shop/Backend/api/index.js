e2a0c089042dd03a9f16cd321fc04a6d2ed31850
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-10-07 22:40:38 +0530

    update

--- Food shop/Backend/api/index.js
+++ Food shop/Backend/api/index.js
@@ -1,7 +1,6 @@
 const express= require('express');
 const path =require ('path');
 const cors=require ('cors');
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +12,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use("/images", express.static(path.join(__dirname, "../public/images")));
-
+app.use("/images", express.static(path.join(__dirname, "../public/../public/images")));
 
 app.get("/", (req, res) => {
   const foodData = [
@@ -22,42 +20,42 @@ app.get("/", (req, res) => {
       name: "Boiled Egg",
       price: 10,
       text: "A simple yet nutritious start to your day.",
-      image: "/images/egg.png",
+      image: "../public/images/egg.png",
       type: "breakfast",
     },
     {
       name: "RAMEN",
       price: 25,
       text: "A quick and flavorful Japanese noodle soup.",
-      image: "/images/ramen.png",
+      image: "../public/images/ramen.png",
       type: "lunch",
     },
     {
       name: "GRILLED CHICKEN",
       price: 45,
       text: "A lean and healthy protein option, perfect for a balanced meal.",
-      image: "/images/chicken.png",
+      image: "../public/images/chicken.png",
       type: "dinner",
     },
     {
       name: "CAKE",
       price: 18,
       text: "Indulge in a sweet treat, perfect for any occasion.",
-      image: "/images/cake.png",
+      image: "../public/images/cake.png",
       type: "breakfast",
     },
     {
       name: "BURGER",
       price: 23,
       text: "A classic American favorite, customizable to your taste.",
-      image: "/images/burger.png",
+      image: "../public/images/burger.png",
       type: "lunch",
     },
     {
       name: "PANCAKE",
       price: 25,
       text: "Fluffy pancakes, a delicious breakfast or brunch option.",
-      image: "/images/pancake.png",
+      image: "../public/images/pancake.png",
       type: "dinner",
     },
   ];
